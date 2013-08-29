d74e150a939e76527587f9dd16091e7cfa8b3281
Philippe Morissette <morisette.philippe@gmail.com>
2013-08-29 00:02:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# nodecec
+
+nodecec is a light wrapper around the cec-client program to ease integration into node.js projects.
+nodecec is currently in it's alpha stage and is still under heavy development. Contributions are more than welcomed. 
+
+# Install 
+
+`npm install nodecec`
---
+++ example/app.js
@@ -0,0 +1,33 @@
+var NodeCEC = require('../index.js')
+
+var cec = new NodeCEC();
+
+// start cec connection
+cec.start();
+
+cec.on('ready', function(data) {
+    console.log("ready...");
+});
+
+cec.on('status', function(data) {
+   console.log("[" + data.id + "] changed from " + data.from + " to " + data.to); 
+});
+
+cec.on('key', function(data) {
+    console.log(data.name);
+});
+
+cec.on('close', function(code) {
+    process.exit(0);
+});
+
+cec.on('error', function(data) {
+    console.log('---------------- ERROR ------------------');
+    console.log(data);
+    console.log('-----------------------------------------');
+});
+
+var stdin = process.openStdin();
+stdin.on('data', function(chunk) { 
+    cec.send(chunk);
+});
